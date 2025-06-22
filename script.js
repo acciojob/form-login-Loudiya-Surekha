6efbbff9c7d62ec2fe225a7e9f35ca1ee82187e3
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-22 11:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+	  event.preventDefault();
+	let form=document.getElementById("form1");
+	let firstName = form.fname.value.trim();
+    let lastName = form.lname.value.trim();
+            alert(`${firstName} ${lastName}`);
 }
